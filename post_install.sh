0daf571d6bece6ec96406358ae8bbfab9fc17a5d
Richard <richard2891@gmail.com>
2022-02-20 22:29:22 -0500

    add user for lazylib.

--- post_install.sh
+++ post_install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -x
 IP_ADDRESS=$(ifconfig | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}')
 
+pw user add lazylib -c lazylibrarian -u 1070 -d /nonexistent -s /usr/bin/nologin
+
 ln -s /usr/local/bin/python3.8 /usr/local/bin/python
 
 pip install apprise
@@ -9,13 +11,13 @@ pip install Pillow
 mkdir /usr/local/lazylibrarian
 git clone https://gitlab.com/LazyLibrarian/LazyLibrarian.git /usr/local/lazylibrarian
 cp /usr/local/lazylibrarian/init/freebsd.initd /usr/local/etc/rc.d/lazylibrarian
-chmod 555 /usr/local/etc/rc.d/lazylibrarian
+chmod 755 /usr/local/etc/rc.d/lazylibrarian
+chown -R lazylib:lazylib /usr/local/lazylibrarian
 
 sysrc lazylibrarian_enable="YES"
-sysrc lazylibrarian_user="root"
-sysrc lazylibrarian_dir="/usr/local/lazylibrarian"
+sysrc lazylibrarian_user="lazylib"
 
 service lazylibrarian start
 
 echo -e "LazyLibrarian now installed.\n" > /root/PLUGIN_INFO
-echo -e "\nGo to $IP_ADDRESS:5299\n" >> /root/PLUGIN_INFO
+echo -e "\nGo to http://$IP_ADDRESS:5299\n" >> /root/PLUGIN_INFO
